cb237087e86935ff6509f9ddc7ff9d031e2ab8fe
Suriyaphet <suriyaphet.inj@gmail.com>
2024-08-13 12:29:28 +0700

    UpdateResponseLayout

---
+++ admin/dashboard/features/components/MobileLayout.js
@@ -0,0 +1,21 @@
+
+const MobileLayout = () => {
+  return (
+    <>
+    <div className='md:hidden grid grid-cols-1 gap-2 w-full border-2 border-black mx-auto justify-items-center h-96'>
+      {/* Content */}
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>1</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>2</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>3</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>4</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>5</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>6</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>7</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>8</div>
+      <div className='bg-blue-500 flex items-center justify-center text-white w-full h-full'>9</div>
+    </div>
+    </>
+  );
+};
+
+export default MobileLayout;
