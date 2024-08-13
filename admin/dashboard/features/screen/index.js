17931b5083bf212b8aa9f042fdc9a2697c8a7560
Suriyaphet <suriyaphet.inj@gmail.com>
2024-08-13 07:10:08 +0700

    fix-h1

--- admin/dashboard/features/screen/index.js
+++ admin/dashboard/features/screen/index.js
@@ -1,8 +1,7 @@
 import DashboardNav from '../components/DashboardNav'
-import { Layout, Row, Col } from 'antd';
-import DashboardFooter from '../components/DashboardFooter';
+import { Layout } from 'antd';
 import DashboardContent from '../components/DashboardContent';
-import styles from '../styles/TetrisGrid.module.css';
+import { Typography } from "antd";  
 
 const DashboardScreen = () => {
 
@@ -13,7 +12,7 @@ const DashboardScreen = () => {
         <DashboardNav />
       
       <DashboardContent>
-
+      <Typography.Title className='text-center'>Desktop</Typography.Title>
       <div className='grid grid-cols-5 grid-rows-4 gap-2 w-full border-2 border-black mx-auto -lg justify-items-center h-96'>
         <div className='bg-blue-500 flex items-center justify-center text-white col-span-3 w-full h-full'>1</div>
         <div className='bg-blue-500 flex items-center justify-center text-white col-span-2 row-span-2 w-full h-full'>2</div>
@@ -27,7 +26,7 @@ const DashboardScreen = () => {
       </div>
       </DashboardContent>
       <DashboardContent>
-
+      <Typography.Title className='text-center'>Tablet</Typography.Title>
       <div className='grid grid-cols-3 grid-rows-8 gap-2 w-full border-2 border-black mx-auto  justify-items-center h-96'>
         <div className='bg-blue-500 flex items-center justify-center text-white col-span-3 w-full h-full'>1</div>
         <div className='bg-blue-500 flex items-center justify-center text-white col-span-1 row-span-8 w-full h-full'>2</div>
@@ -41,7 +40,7 @@ const DashboardScreen = () => {
       </div>
       </DashboardContent>
       <DashboardContent>
-
+      <Typography.Title className='text-center'>Mobile</Typography.Title>
       <div className='grid grid-cols-1  gap-2 w-full border-2 border-black mx-auto  justify-items-center h-96'>
         <div className='bg-blue-500 flex items-center justify-center text-white  w-full h-full'>1</div>
         <div className='bg-blue-500 flex items-center justify-center text-white  w-full h-full'>2</div>
