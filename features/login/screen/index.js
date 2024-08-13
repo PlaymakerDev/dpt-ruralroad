f8112357b0df92ca2c36f26f92a24492a8d59144
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2024-08-13 12:04:10 +0700

    update login page

--- features/login/screen/index.js
+++ features/login/screen/index.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { FormLogin, PageImage } from '../components/content'
+import { FormLogin } from '../components/content'
 import styles from '@/features/login/styles/Login.module.css'
 import { Col, Row } from 'antd'
 import Image from 'next/image'
 import PageBanner from '@/public/images/login-img.png'
+import DPTTop from '@/public/images/top-line.svg'
+import DPTBottom from '@/public/images/bottom-line.svg'
 
 const LoginScreen = (props) => {
   const { } = props
 
   return (
-    <main className='p-10 w-full h-screen lg:overflow-hidden'>
-      <Row gutter={[80, 30]} align={'middle'} className={styles.rowContainer}>
+    <main className={`${styles.mainContainer} relative z-10`}>
+      <Row gutter={[0, 30]} justify={'center'} align={'middle'} className={styles.rowContainer}>
         <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12}>
-          <section className='mx-auto max-w-xl'>
+          <section className={styles.loginContainer}>
             <FormLogin />
           </section>
         </Col>
         <Col xs={24} sm={24} md={24} lg={24} xl={12} xxl={12} className={styles.imageColumn}>
-          <section className='mx-auto'>
+          <section className='p-12'>
             <Image
               src={PageBanner}
               alt='login-banner'
@@ -26,6 +28,18 @@ const LoginScreen = (props) => {
           </section>
         </Col>
       </Row>
+      <Image
+        src={DPTTop}
+        alt='dpt-top'
+        width={548}
+        height={274}
+        className='absolute top-0 left-0 -z-10'
+      />
+      <Image
+        src={DPTBottom}
+        alt='dpt-bottom'
+        className='absolute bottom-10 translate-x-3/4 -z-10'
+      />
     </main>
   )
 }
