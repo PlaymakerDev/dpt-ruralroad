f33c3c2ad2dc1f899c77efd9545b6ba7a455d912
OXMMEE <suvitcha.jitthai@gmail.com>
2024-08-15 14:36:07 +0700

    OhmMee Update WeighInfomationTable V.1
    Mai Mee Ah Rai I KaJock

---
+++ features/weighinginfo/screen/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FieldTable from '../components/fieldtable'
+import Weightab from '../components/weightab'
+import Weightable from '../components/weightable'
+
+const WeighinfoPage = () => {
+  return (
+    <div>
+      <Weightab />
+    </div>
+  )
+}
+
+export default WeighinfoPage
---
+++ pages/weighinginfo/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WeighinfoPage from '@/features/weighinginfo/screen'
+import { PageLayout } from '@/components/layout'
+
+const Weighinginfo = () => {
+  return (
+    <PageLayout >
+      <WeighinfoPage />
+    </PageLayout>
+  )
+}
+
+export default Weighinginfo
