4c3654c1de2dca0d6d833f00c21181c91dfaf1a2
Suriyaphet <suriyaphet.inj@gmail.com>
2024-08-19 16:48:27 +0700

    mini fuking update

--- features/orgsetupplan/components/modal/ResponseModal.js
+++ features/orgsetupplan/components/modal/ResponseModal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Typography, Button, Modal, Row } from 'antd';
 import ErrorIco from '@/public/ErrorIco';
 import SuccessIco from '@/public/SuccessIco';
