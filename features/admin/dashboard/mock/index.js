4a8919ff9ca87ff9a844467b4e303208abf91146
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-04-21 09:21:44 +0700

    mock data added

---
+++ features/admin/dashboard/mock/index.js
@@ -0,0 +1,193 @@
+export const MAP_PIN = {
+  "station": [
+    {
+      "StationID": 1,
+      "Latitude": "13.474057",
+      "Longtitude": "100.070831"
+    },
+    {
+      "StationID": 2,
+      "Latitude": "13.925742",
+      "Longtitude": "100.453386"
+    },
+    {
+      "StationID": 5,
+      "Latitude": "18.617229",
+      "Longtitude": "98.836676"
+    }
+  ],
+  "wim": [
+    {
+      "StationID": 3,
+      "Latitude": "12.904348",
+      "Longtitude": "101.149446"
+    },
+    {
+      "StationID": 4,
+      "Latitude": "19.988391",
+      "Longtitude": "99.891782"
+    },
+    {
+      "StationID": 5,
+      "Latitude": "13.396447",
+      "Longtitude": "99.748062"
+    },
+    {
+      "StationID": 8,
+      "Latitude": "16.648881",
+      "Longtitude": "104.712323"
+    },
+    {
+      "StationID": 9,
+      "Latitude": "16.769887",
+      "Longtitude": "102.685576"
+    },
+    {
+      "StationID": 11,
+      "Latitude": "16.700625",
+      "Longtitude": "100.257536"
+    },
+    {
+      "StationID": 101,
+      "Latitude": "13.652911699024894",
+      "Longtitude": "100.54130135114752"
+    },
+    {
+      "StationID": 102,
+      "Latitude": "10.583973",
+      "Longtitude": "99.169432"
+    },
+    {
+      "StationID": 103,
+      "Latitude": "13.062549078191589",
+      "Longtitude": "102.43517081081761"
+    },
+    {
+      "StationID": 104,
+      "Latitude": "13.884081553785949",
+      "Longtitude": "100.28697023861163"
+    },
+    {
+      "StationID": 105,
+      "Latitude": "13.685631",
+      "Longtitude": "100.546786"
+    },
+    {
+      "StationID": 106,
+      "Latitude": "13.828526",
+      "Longtitude": "100.486900"
+    },
+    {
+      "StationID": 107,
+      "Latitude": "13.828526",
+      "Longtitude": "100.486900"
+    },
+    {
+      "StationID": 108,
+      "Latitude": "13.665335",
+      "Longtitude": "100.520272"
+    },
+    {
+      "StationID": 109,
+      "Latitude": "13.923801621165756",
+      "Longtitude": "100.47393686501762"
+    },
+    {
+      "StationID": 110,
+      "Latitude": "17.39190417343345",
+      "Longtitude": "104.6879030485681"
+    },
+    {
+      "StationID": 111,
+      "Latitude": "16.729177641747533",
+      "Longtitude": "98.51625826239166"
+    },
+    {
+      "StationID": 112,
+      "Latitude": "16.729177641747533",
+      "Longtitude": "98.51625826239166"
+    },
+    {
+      "StationID": 113,
+      "Latitude": "13.854478",
+      "Longtitude": "100.496889"
+    },
+    {
+      "StationID": 114,
+      "Latitude": "14.33287282246893",
+      "Longtitude": "100.59227844992864"
+    },
+    {
+      "StationID": 115,
+      "Latitude": "10.4587027",
+      "Longtitude": "99.1358552"
+    },
+    {
+      "StationID": 116,
+      "Latitude": "18.743598",
+      "Longtitude": "98.976633"
+    },
+    {
+      "StationID": 117,
+      "Latitude": "20.033006",
+      "Longtitude": " 99.955460"
+    },
+    {
+      "StationID": 118,
+      "Latitude": "13.06282",
+      "Longtitude": "100.9567146"
+    },
+    {
+      "StationID": 119,
+      "Latitude": "13.844265",
+      "Longtitude": "100.459195"
+    },
+    {
+      "StationID": 201,
+      "Latitude": "13.799947",
+      "Longtitude": "100.1987736"
+    },
+    {
+      "StationID": 202,
+      "Latitude": "13.799947",
+      "Longtitude": "100.1987736"
+    },
+    {
+      "StationID": 203,
+      "Latitude": "18.591086",
+      "Longtitude": "98.836637"
+    },
+    {
+      "StationID": 204,
+      "Latitude": "20.008219",
+      "Longtitude": "99.893258"
+    },
+    {
+      "StationID": 205,
+      "Latitude": "19.145733",
+      "Longtitude": "99.947550"
+    },
+    {
+      "StationID": 206,
+      "Latitude": "15.05437",
+      "Longtitude": "102.17931"
+    },
+    {
+      "StationID": 207,
+      "Latitude": "14.93336",
+      "Longtitude": "102.14238"
+    }
+  ],
+  "mobile": [
+    {
+      "TID": "17702",
+      "Latitude": "14.260761346680644",
+      "Longtitude": "99.93555069910356"
+    },
+    {
+      "TID": "17765",
+      "Latitude": "14.3789923",
+      "Longtitude": "100.5682019"
+    }
+  ]
+}
