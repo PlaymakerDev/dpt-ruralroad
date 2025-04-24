a19c8ec34afb844e16a5f7bac50b12e3bcc24ae5
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-04-24 16:25:41 +0700

    fix(dashboard): temporary removed map

--- features/admin/dashboard/screen/index.js
+++ features/admin/dashboard/screen/index.js
@@ -1,12 +1,11 @@
 "use client"
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React from 'react'
 import { Button, Col, Row, Spin } from 'antd'
 import { CardOverWeightVehicle, CardVehicleStat, CardWeighingStation } from '../components/addon-content/card'
 import { CollapseYearSummary } from '../components/addon-content/collapse'
 // CREATE API REQUESTS
 // import { useAppSelector } from '@/store/hooks'
 // import { allowAdmin } from '@/utils/allowAdmin'
-import { longdo, map, LongdoMap } from '@/components/map/LongdoMap'
 
 const MAP_KEY = "f7ba675880ccab7ac7fd0a65f1b33553"
 
@@ -14,49 +13,6 @@ const DashboardScreen = (props) => {
   const { } = props
   // USE SELECTOR
   // const user = useAppSelector(state => state.user)
-
-  const initMap = useCallback(() => {
-    // map.Layers.setBase(longdo.Layers.GRAY);
-
-    longdo.Map({
-      placeholder: "sample",
-      language: 'th',
-      lastView: false,
-      zoom: 13, // เพิ่มค่า zoom เริ่มต้น
-      // zoomRange: { min: 12, max: 14 },
-
-      mouse: {
-        wheel: false, // ปิดการซูมด้วยลูกกลิ้งเมาส์
-      },
-    })
-
-    longdo.Marker({ lon: 101.2, lat: 12.8 },
-      // {
-      //   title: 'Marker',
-      //   icon: {
-      //     url: 'https://map.longdo.com/mmmap/images/pin_mark.png',
-      //     offset: { x: 12, y: 45 }
-      //   },
-      //   detail: 'Drag me',
-      //   visibleRange: { min: 7, max: 9 },
-      //   draggable: true,
-      //   weight: longdo.OverlayWeight.Top,
-      // }
-    )
-
-    // ตั้งค่าพื้นฐานของแผนที่
-    map.Layers.setBase(longdo.Layers.NORMAL)
-
-    // แสดงเครื่องมือบนแผนที่
-    map.Ui.DPad.visible(false)
-    map.Ui.Zoombar.visible(false)
-    map.Ui.Geolocation.visible(false)
-    map.Ui.Toolbar.visible(false)
-    map.Ui.LayerSelector.visible(false)
-    map.Ui.Fullscreen.visible(false)
-    map.Ui.Crosshair.visible(false)
-    map.Ui.Scale.visible(false)
-  }, []);
 
   return (
     <>
@@ -77,7 +33,7 @@ const DashboardScreen = (props) => {
               <CardOverWeightVehicle />
             </Col>
             <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={20}>
-              <LongdoMap id="longdo-map" mapKey={MAP_KEY} callback={initMap} />
+              Test
             </Col>
           </Row>
         </section>
