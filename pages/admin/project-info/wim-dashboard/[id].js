18eccffd398232bddb87c16eac0f9e56d30af62d
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-04-26 17:45:06 +0700

    fix(project-info): applied serversideprops

--- pages/admin/project-info/wim-dashboard/[id].js
+++ pages/admin/project-info/wim-dashboard/[id].js
@@ -3,6 +3,11 @@ import { useRouter } from 'next/router'
 import WIMDashboardScreen from '@/features/admin/project-info/wim-dashboard/screen'
 import PageLayout from '@/components/layout/new-layout/PageLayout'
 import { Breadcrumb } from 'antd'
+// CHECK ROLE
+import { getLoginSession } from '@/utils/auth'
+import { validatePermissionRoute, redirectToLogin, sessionToProps } from '@/utils/auth/routePermission'
+import { wrapper, AppState } from '@/store'
+import { signIn } from '@/store/features/userSlice'
 
 const WIMDashboardPage = (props) => {
 	const { } = props
@@ -32,5 +37,18 @@ const WIMDashboardPage = (props) => {
 		</PageLayout>
 	)
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(store => (async (context) => {
+	const [session, redirectPath] = await getLoginSession(context.req, context.res)
+
+	const valid = validatePermissionRoute(session, ['ADMIN', 'USER'])
+	if (!valid) {
+		return redirectToLogin(redirectPath)
+	}
+
+	store.dispatch(signIn(session));
+
+	return sessionToProps(session, session?.message)
+}));
 
 export default React.memo(WIMDashboardPage)
--- pages/admin/project-info/wim-detail/[id].js
+++ pages/admin/project-info/wim-detail/[id].js
@@ -3,6 +3,11 @@ import { useRouter } from 'next/router'
 import WIMDetailScreen from '@/features/admin/project-info/wim-detail/screen'
 import PageLayout from '@/components/layout/new-layout/PageLayout'
 import { Breadcrumb } from 'antd'
+// CHECK ROLE
+import { getLoginSession } from '@/utils/auth'
+import { validatePermissionRoute, redirectToLogin, sessionToProps } from '@/utils/auth/routePermission'
+import { wrapper, AppState } from '@/store'
+import { signIn } from '@/store/features/userSlice'
 
 const WIMDetailPage = (props) => {
   const { } = props
@@ -38,5 +43,18 @@ const WIMDetailPage = (props) => {
     </PageLayout>
   )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(store => (async (context) => {
+  const [session, redirectPath] = await getLoginSession(context.req, context.res)
+
+  const valid = validatePermissionRoute(session, ['ADMIN', 'USER'])
+  if (!valid) {
+    return redirectToLogin(redirectPath)
+  }
+
+  store.dispatch(signIn(session));
+
+  return sessionToProps(session, session?.message)
+}));
 
 export default React.memo(WIMDetailPage)
