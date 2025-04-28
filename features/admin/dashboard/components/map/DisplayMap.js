e659499f94304790b3722ed03f40d278e99ed63e
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-04-28 09:40:57 +0700

    fix(map): removed unused code

--- features/admin/dashboard/components/map/DisplayMap.js
+++ features/admin/dashboard/components/map/DisplayMap.js
@@ -7,50 +7,46 @@ const MAP_KEY = "f7ba675880ccab7ac7fd0a65f1b33553"
 const DisplayMap = (props) => {
   const { } = props
   const [display, setDisplay] = useState(false)
-  const mapRef = useRef()
 
-  console.log("===", MAP_PIN.station.map(item => {
-    return {
-      lon: item.Longtitude,
-      lat: item.Latitude,
-    }
-  }))
+  // console.log("===", MAP_PIN.station.map(item => {
+  //   return {
+  //     lon: item.Longtitude,
+  //     lat: item.Latitude,
+  //   }
+  // }))
 
   const initMap = useCallback(() => {
     if (map && longdo) {
-      setDisplay(true)
-      // map.Layers.setBase(longdo.Layers.GRAY);
-      new window.longdo.Map({
-        placeholder: '',
-        language: 'th',
-        lastView: false,
-        zoom: 13, // เพิ่มค่า zoom เริ่มต้น
-        // zoomRange: { min: 12, max: 14 },
+      map.Layers.setBase(longdo.Layers.GRAY);
+      // new window.longdo.Map({
+      //   placeholder: '',
+      //   language: 'th',
+      //   lastView: false,
+      //   zoom: 13, // เพิ่มค่า zoom เริ่มต้น
+      //   // zoomRange: { min: 12, max: 14 },
 
-        mouse: {
-          wheel: false, // ปิดการซูมด้วยลูกกลิ้งเมาส์
-        }
-      })
-      // ตั้งค่าพื้นฐานของแผนที่
-      map.Layers.setBase(window.longdo.Layers.NORMAL)
+      //   mouse: {
+      //     wheel: false, // ปิดการซูมด้วยลูกกลิ้งเมาส์
+      //   }
+      // })
+      // // ตั้งค่าพื้นฐานของแผนที่
+      // map.Layers.setBase(window.longdo.Layers.NORMAL)
 
-      // แสดงเครื่องมือบนแผนที่
-      map.Ui.DPad.visible(false)
-      map.Ui.Zoombar.visible(false)
-      map.Ui.Geolocation.visible(false)
-      map.Ui.Toolbar.visible(false)
-      map.Ui.LayerSelector.visible(false)
-      map.Ui.Fullscreen.visible(false)
-      map.Ui.Crosshair.visible(false)
-      map.Ui.Scale.visible(false)
+      // // แสดงเครื่องมือบนแผนที่
+      // map.Ui.DPad.visible(false)
+      // map.Ui.Zoombar.visible(false)
+      // map.Ui.Geolocation.visible(false)
+      // map.Ui.Toolbar.visible(false)
+      // map.Ui.LayerSelector.visible(false)
+      // map.Ui.Fullscreen.visible(false)
+      // map.Ui.Crosshair.visible(false)
+      // map.Ui.Scale.visible(false)
 
       // Add other map configurations here
-      MAP_PIN.station.map(item => {
-        map.Overlays.add(new longdo.Marker({ lon: item.Longtitude, lat: item.Latitude }));
-      })
+      // MAP_PIN.station.map(item => {
+      //   map.Overlays.add(new longdo.Marker({ lon: item.Longtitude, lat: item.Latitude }));
+      // })
     }
-
-    setDisplay(false)
   }, [MAP_PIN, map, longdo, display, setDisplay])
 
   // const loadLongdoMapScript = useCallback(() => {
@@ -71,21 +67,17 @@ const DisplayMap = (props) => {
   //   document.head.appendChild(script)
   // }, [])
 
-  useEffect(() => {
-    initMap()
-  }, [])
+  // useEffect(() => {
+  //   initMap()
+  // }, [])
 
   return (
     <>
-    <button onClick={() => initMap()}>Loadmap</button>
-      {display ?
-        <LongdoMap
-          id="longdo-map"
-          mapKey={MAP_KEY}
-          callback={initMap()}
-        />
-        : null
-      }
+      <LongdoMap
+        id="longdo-map"
+        mapKey={MAP_KEY}
+        callback={initMap()}
+      />
     </>
   )
 }
