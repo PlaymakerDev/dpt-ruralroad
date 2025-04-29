396865dd2f98d7022dbf7d712dec862856b0b1ff
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-04-29 17:56:26 +0700

    fix(auth): update auth utils

--- utils/auth/routePermission.js
+++ utils/auth/routePermission.js
@@ -1,42 +1,31 @@
-import config from '@/config'
-import { getLoginSession } from '@/utils/auth'
-
-export async function validatePermissionRoute(req, res) {
-  try {
-    console.log('validatePermissionRoute called');
-    const session = await getLoginSession(req)
-    console.log('Session in validatePermissionRoute:', session ? 'exists' : 'null');
-    
-    return session;
-  } catch (error) {
-    console.error('Error in validatePermissionRoute:', error);
-    return null;
+export function validatePermissionRoute(user, roles) {
+  let canView = false
+  if (user?.token) {
+    if (user.token && roles.includes(user.map_group_name)) {
+      canView = true
+    }
   }
+
+  return canView
 }
 
 export function redirectToLogin(path = '/login') {
+  let destinationPath = path
   return {
     redirect: {
-      destination: path,
+      destination: destinationPath,
       permanent: false,
     },
   }
 }
 
-export function validateDeptType(){
-
-}
-
-export function sessionToProps(session, message, status) {
-  // แปลง message เป็น string หรือใช้ค่า null
-  const errorMessage = message ? message.toString() : null;
-  
+export function sessionToProps(session, message) {
   return {
     props: {
-      session: session || null,
-      error: errorMessage,
-      // เก็บข้อมูล status ไว้ใน session ถ้าจำเป็น
-      ...(session && status ? { status } : {}),
+      user: session || null,
+      error: {
+        message: message || null,
+      },
     },
   }
 }
