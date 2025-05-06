8ccfb33cbcddd975273a3e2f960027ca5997def0
DESKTOP-PS25VHU\DELL <poracademic2020@gmail.com>
2025-05-06 15:09:04 +0700

    feat: added FormSearchCCTV

---
+++ features/admin/cctv/overview/components/FormSearchCCTV.js
@@ -0,0 +1,73 @@
+import React, { useCallback, useMemo, useRef } from 'react'
+import { Form, Field, useForm } from "@/components/form";
+
+const FormSearchCCTV = (props) => {
+  const { dptGroup, defaultSearch, apiGetData, cctvRef } = props
+  const submitRef = useRef()
+
+  const form = useForm({
+    initialValues: {
+      department_id: ''
+    },
+    rules: {}
+  })
+
+  const { handlerChange } = form;
+
+  const strDptGroup = useMemo(() => {
+    const updateArr = dptGroup?.map((item) => {
+      return {
+        ...item,
+        department_id: String(item.department_id)
+      }
+    })
+    return updateArr
+  }, [dptGroup])
+
+  const onChangeDptGroup = useCallback(async (name, value) => {
+    await handlerChange({
+      [name]: String(value)
+    })
+    await submitRef.current?.click()
+  }, [])
+
+  const buildValue = useCallback((values, next) => {
+    const body = {
+      department_id: values.department_id
+    }
+    next(body)
+  }, [])
+
+  const handlerSubmit = useCallback((values) => {
+    cctvRef.current = false
+    apiGetData('/api/v1/cctv/deparment_list_sum', {
+      ...defaultSearch,
+      ...values,
+    }, false)
+  }, [])
+
+  return (
+    <Form
+      form={form}
+      handlerSubmit={[buildValue, handlerSubmit]}
+    >
+      <Field.Select
+        label='หน่วยงาน'
+        name='department_id'
+        placeholder='หน่วยงาน'
+        optKeys={['department_id', 'department_name']}
+        options={strDptGroup}
+        allowClear
+        // SEARCHABLE
+        showSearch
+        optionFilterProp="children"
+        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+        hideRequired
+        onChange={(name, value) => onChangeDptGroup(name, value)}
+      />
+      <button ref={submitRef} type="submit" hidden />
+    </Form>
+  )
+}
+
+export default React.memo(FormSearchCCTV)
