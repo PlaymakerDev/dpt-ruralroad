b90e2cfddcd472e3596c661b9c5f79a9cee86b2f
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-05-15 05:07:00 +0700

    fix: adjust map2

--- components/map/Map2.js
+++ components/map/Map2.js
@@ -1,6 +1,5 @@
 "use client";
-
-import React/*, { useEffect, useMemo, useState }*/ from "react";
+import React, { useEffect }/*, { useEffect, useMemo, useState }*/ from "react";
 // IMPORTANT: the order matters!
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
@@ -9,34 +8,34 @@ import "leaflet-routing-machine";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polyline, useMap } from 'react-leaflet'
 // import { createControlComponent } from '@react-leaflet/core'
 
-
 const Map = (props) => {
   const {
     center,
     zoom,
-    renderLocationMarker = null,
-    renderRoutingMachine = null,
+    children,
+    ...mapProps
   } = props
 
   return (
     <MapContainer
       center={center}
       zoom={zoom}
-      scrollWheelZoom={true}
+      scrollWheelZoom={false}
       style={{
         width: "100%",
         height: "100%",
-        minHeight: "40rem",
+        // minHeight: "496px",
+        minHeight: "31rem",
         border: 0
       }}
       className="!rounded-lg"
+      {...mapProps}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {renderLocationMarker}
-      {renderRoutingMachine}
+      {children}
     </MapContainer>
   );
 }
