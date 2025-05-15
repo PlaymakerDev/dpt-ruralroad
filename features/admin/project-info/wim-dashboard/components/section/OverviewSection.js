709ec612041969ccf95e680bde70b18bc3111b36
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-05-15 09:49:51 +0700

    fix: uncomment dashboard

--- features/admin/project-info/wim-dashboard/components/section/OverviewSection.js
+++ features/admin/project-info/wim-dashboard/components/section/OverviewSection.js
@@ -7,14 +7,14 @@ const OverviewSection = (props) => {
 
   return (
     <div>
-      {/* <section>
+      <section>
         <figure className='flex-1 h-[calc(100vh-302px)] min-h-[400px]'>
           <DisplayMap
             stationId={data?.id}
             stationType={data?.type}
           />
         </figure>
-      </section> */}
+      </section>
       <section className='mt-5'>
         <Stat
           stationId={data?.id}
