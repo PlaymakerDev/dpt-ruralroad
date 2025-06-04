235cb5cfe5cf601a11e8c17009b72a3464acdf70
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-06-04 16:04:13 +0700

    feat: added gps route

---
+++ pages/admin/gps/overview/index.js
@@ -0,0 +1,45 @@
+import React, { useMemo } from 'react'
+import PageLayout from '@/components/layout/new-layout/PageLayout'
+import OverviewScreen from '@/features/admin/information/vehicle-data-on-routes/overview/screen'
+import { Breadcrumb } from 'antd'
+// CHECK ROLE
+import { getLoginSession } from '@/utils/auth'
+import { validatePermissionRoute, redirectToLogin, sessionToProps } from '@/utils/auth/routePermission'
+import { wrapper, AppState } from '@/store'
+import { signIn } from '@/store/features/userSlice'
+
+const OverviewPage = (props) => {
+  const { } = props
+
+  const renderBreadcrumb = useMemo(() => {
+    return (
+      <Breadcrumb separator='>'>
+        <Breadcrumb.Item className='text-gray-500'>ข้อมูล</Breadcrumb.Item>
+        <Breadcrumb.Item className='font-bold'>ข้อมูลรถ GPS</Breadcrumb.Item>
+      </Breadcrumb>
+    )
+  }, [])
+
+  return (
+    <PageLayout
+      breadcrumb={renderBreadcrumb}
+    >
+      <OverviewScreen />
+    </PageLayout>
+  )
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(store => (async (context) => {
+  const session = await getLoginSession(context.req)
+
+  const valid = validatePermissionRoute(session, ['ADMIN', 'USER'])
+  if (!valid) {
+    return redirectToLogin()
+  }
+
+  store.dispatch(signIn(session));
+
+  return sessionToProps(session, session?.message)
+}));
+
+export default React.memo(OverviewPage)
---
+++ pages/admin/gps/view/[id].js
@@ -0,0 +1,50 @@
+import React, { useMemo } from 'react'
+import PageLayout from '@/components/layout/new-layout/PageLayout'
+import ViewScreen from '@/features/admin/information/vehicle-data-on-routes/view/screen'
+import { useRouter } from 'next/router'
+import { Breadcrumb } from 'antd'
+// CHECK ROLE
+import { getLoginSession } from '@/utils/auth'
+import { validatePermissionRoute, redirectToLogin, sessionToProps } from '@/utils/auth/routePermission'
+import { wrapper, AppState } from '@/store'
+import { signIn } from '@/store/features/userSlice'
+
+const ViewPage = (props) => {
+  const { } = props
+  const { query, push } = useRouter()
+
+  const renderBreadcrumb = useMemo(() => {
+    return (
+      <Breadcrumb separator='>'>
+        <Breadcrumb.Item className='text-gray-500'>ข้อมูล</Breadcrumb.Item>
+        <Breadcrumb.Item className='text-gray-500 cursor-pointer hover:text-white transition duration-300' onClick={() => push('/admin/gps/overview')}>ข้อมูลรถ GPS</Breadcrumb.Item>
+        <Breadcrumb.Item className='font-bold'>รายละเอียดข้อมูลรถ</Breadcrumb.Item>
+      </Breadcrumb>
+    )
+  }, [push])
+
+  return (
+    <PageLayout
+      breadcrumb={renderBreadcrumb}
+    >
+      <ViewScreen
+        id={query.id || '1'}
+      />
+    </PageLayout>
+  )
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(store => (async (context) => {
+  const session = await getLoginSession(context.req)
+
+  const valid = validatePermissionRoute(session, ['ADMIN', 'USER'])
+  if (!valid) {
+    return redirectToLogin()
+  }
+
+  store.dispatch(signIn(session));
+
+  return sessionToProps(session, session?.message)
+}));
+
+export default React.memo(ViewPage)
